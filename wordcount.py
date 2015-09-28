57a762ee7884ee736d77488c06e096bf4c82a820
Hackbright Student <info@hackbrightacademy.com>
2015-09-28 13:07:09 -0700

    added function

--- wordcount.py
+++ wordcount.py
@@ -1,18 +1,19 @@
 # put your code here.
-text_file = open("test.txt")
-word_count = {}
-for line in text_file:
-    strip_line = line.strip()
-    poem = strip_line.split(' ')
-    for word in poem: 
-        if word in word_count:
-            word_count[word] = word_count[word] +1
-        elif word not in word_count:
-            word_count[word] = 1
-text_file.close()
+def counting_words(file_name):
+    text_file = open(file_name)
+    word_count = {}
+    for line in text_file:
+        strip_line = line.strip()
+        poem = strip_line.split(' ')
+        for word in poem: 
+            if word in word_count:
+                word_count[word] = word_count[word] +1
+            elif word not in word_count:
+                word_count[word] = 1
+    text_file.close()
 
-for word in word_count:
-    print word, word_count[word]
+    for word in word_count:
+        print word, word_count[word]
 
 
-
+counting_words("twain.txt")
