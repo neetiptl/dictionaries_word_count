1e36d41be4aaab67eaeab58ed494bef292b6e0a1
Hackbright Student <info@hackbrightacademy.com>
2015-09-28 17:22:01 -0700

    further study

--- wordcount.py
+++ wordcount.py
@@ -1,11 +1,22 @@
 # put your code here.
+import sys
+
+file_name = sys.argv[1]
+
 def counting_words(file_name):
     text_file = open(file_name)
     word_count = {}
+    puctuation = ',.?!@#$%^&*~-:;-_"'
+    
     for line in text_file:
         strip_line = line.strip()
-        poem = strip_line.split(' ')
-        for word in poem: 
+        poem = strip_line.split()
+        for word in poem:
+            no_punct =""
+            for char in word:
+                if char not in puctuation:
+                    no_punct = no_punct + char
+            word = no_punct.lower()
             if word in word_count:
                 word_count[word] = word_count[word] +1
             elif word not in word_count:
@@ -16,4 +27,4 @@ def counting_words(file_name):
         print word, word_count[word]
 
 
-counting_words("twain.txt")
+counting_words(file_name)
